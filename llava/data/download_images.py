5bcf3554ac94f10487a8b4d4465107296cc0cbfb
kelo2000 <ltl2113@nyu.edu>
2024-01-22 23:14:24 +0400

    Changes on data download

--- llava/data/download_images.py
+++ llava/data/download_images.py
@@ -1,49 +1,171 @@
+# import os
+# import json
+# import shutil
+# import tarfile
+# import argparse
+# import logging
+# import urllib.request
+# from urllib.error import HTTPError, URLError
+# from tqdm import tqdm
+
+# def download_file(url, output_path, max_retries=3):
+#     retries = 0
+#     while retries < max_retries:
+#         try:
+#             urllib.request.urlretrieve(url, output_path)
+#             return True
+#         except (HTTPError, URLError) as e:
+#             logging.warning(f"Retry {retries+1}: Error downloading {url} - {e}")
+#             retries += 1
+#     return False
+
+# def main(args):
+#     input_data = []
+#     with open(args.input_path) as f:
+#         for line in f:
+#             input_data.append(json.loads(line))
+
+#     # Download all PMC articles
+#     logging.info('Downloading PMC articles')
+#     for sample in tqdm(input_data):
+#         pmc_tar_url = sample['pmc_tar_url']
+#         output_path = os.path.join(args.pmc_output_path, os.path.basename(pmc_tar_url))
+#         if not download_file(pmc_tar_url, output_path):
+#             logging.error(f'Failed to download {pmc_tar_url} after retries')
+
+#     # Untar all PMC articles
+#     logging.info('Untarring PMC articles')
+#     for sample in tqdm(input_data):
+#         fname = os.path.join(args.pmc_output_path, os.path.basename(sample['pmc_tar_url']))
+#         if os.path.exists(fname):
+#             try:
+#                 with tarfile.open(fname, "r:gz") as tar:
+#                     tar.extractall(args.pmc_output_path)
+#             except tarfile.TarError as e:
+#                 logging.error(f"Error extracting {fname}: {e}")
+
+#     # Copy to images directory
+#     logging.info('Copying images')
+#     for sample in tqdm(input_data):
+#         src = os.path.join(args.pmc_output_path, sample['image_file_path'])
+#         dst = os.path.join(args.images_output_path, sample['pair_id']+'.jpg')
+#         if os.path.exists(src) and os.path.isfile(src):
+#             shutil.copyfile(src, dst)
+#         else:
+#             logging.warning(f"Image file not found or invalid: {src}")
+
+# if __name__ == '__main__':
+#     logging.basicConfig(level=logging.INFO)
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--input_path', type=str, default='data/llava_med_image_urls.jsonl')
+#     parser.add_argument('--pmc_output_path', type=str, default='data/pmc_articles/')
+#     parser.add_argument('--images_output_path', type=str, default='data/images/')
+#     args = parser.parse_args()
+#     main(args)
+
+# import os
+# import json
+# import shutil
+# from tqdm import tqdm
+# import tarfile
+# import argparse
+# from urllib.error import HTTPError
+
+# import urllib.request
+# from concurrent.futures import ThreadPoolExecutor, as_completed
+
+# def download(url, output_path):
+#     try:
+#         urllib.request.urlretrieve(url, output_path)
+#     except (HTTPError) as e:
+#         pass
+#         #logging.warning(f"Retry {retries+1}: Error downloading {url} - {e}")
+
+
+# def main(args):
+#     input_data = []
+#     with open(args.input_path) as f:
+#         for line in f:
+#             input_data.append(json.loads(line))
+
+#     # Download all PMC articles
+#     print('Downloading PMC articles')
+#     with ThreadPoolExecutor(max_workers=30) as executor:
+#         results = [executor.submit(download, sample['pmc_tar_url'], os.path.join(args.pmc_output_path, os.path.basename(sample['pmc_tar_url']))) for sample in input_data] 
+#         for future in tqdm(as_completed(results), total=len(input_data)):
+#             future.result()
+
+#     # Untar all PMC articles
+#     print('Untarring PMC articles')
+#     for sample in tqdm(input_data):
+#         fname = os.path.join(args.pmc_output_path, os.path.basename(os.path.join(sample['pmc_tar_url'])))
+#         tar = tarfile.open(fname, "r:gz")
+#         tar.extractall(args.pmc_output_path)
+#         tar.close()
+        
+#     # Copy to images directory
+#     print('Copying images')
+#     for sample in tqdm(input_data):
+#         src = os.path.join(args.pmc_output_path, sample['image_file_path'])
+#         dst = os.path.join(args.images_output_path, sample['pair_id']+'.jpg')
+#         shutil.copyfile(src, dst)
+      
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--input_path', type=str, default='data/llava_med_image_urls.jsonl')
+#     parser.add_argument('--pmc_output_path', type=str, default='data/pmc_articles/')
+#     parser.add_argument('--images_output_path', type=str, default='data/images/')
+#     args = parser.parse_args()
+#     main(args)
+
+
 import os
 import json
 import shutil
-from tqdm import tqdm
 import tarfile
 import argparse
 from urllib.error import HTTPError
 import urllib.request
+from multiprocessing import Pool
+import multiprocessing as mp
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_path', type=str, default='data/llava_med_image_urls.jsonl')
+parser.add_argument('--pmc_output_path', type=str, default='data/pmc_articles/')
+parser.add_argument('--images_output_path', type=str, default='data/images/')
+parser.add_argument('--remove_pmc', action='store_true', default=False, help='remove pmc articles after image extraction')
+parser.add_argument('--cpus', type=int, default=-1, help='number of cpus to use in multiprocessing (default: all)')
+args = parser.parse_args()
 
-def main(args):
-  input_data = []
-  with open(args.input_path) as f:
+input_data = []
+with open(args.input_path) as f:
     for line in f:
-      input_data.append(json.loads(line))
+        input_data.append(json.loads(line))
 
-  # Download all PMC articles
-  print('Downloading PMC articles')
-  for idx, sample in enumerate(tqdm(input_data)):
+def download_func(idx):
+    sample = input_data[idx]
     try:
-      urllib.request.urlretrieve(sample['pmc_tar_url'], os.path.join(args.pmc_output_path, os.path.basename(sample['pmc_tar_url'])))
-    except HTTPError as e:
-      print('Error downloading PMC article: {}'.format(sample['pmc_tar_url']))
-      continue
-
-
-  # Untar all PMC articles
-  print('Untarring PMC articles')
-  for sample in tqdm(input_data):
-    fname = os.path.join(args.pmc_output_path, os.path.basename(os.path.join(sample['pmc_tar_url'])))
-    tar = tarfile.open(fname, "r:gz")
-    tar.extractall(args.pmc_output_path)
-    tar.close()
+        urllib.request.urlretrieve(sample['pmc_tar_url'], os.path.join(args.pmc_output_path, os.path.basename(sample['pmc_tar_url'])))
+        fname = os.path.join(args.pmc_output_path, os.path.basename(os.path.join(sample['pmc_tar_url'])))
+        
+        tar = tarfile.open(fname, "r:gz")
+        tar.extractall(args.pmc_output_path)
+        tar.close()
+        src = os.path.join(args.pmc_output_path, sample['image_file_path'])
+        dst = os.path.join(args.images_output_path, sample['pair_id']+'.jpg')
+        shutil.copyfile(src, dst)  
+        if args.remove_pmc:
+            os.remove(fname)
+            shutil.rmtree(os.path.join(args.pmc_output_path, str(os.path.basename(sample['pmc_tar_url']))).split('.tar.gz')[0]+'/')
+    except Exception as e:
+        print(e)
+        
+if args.cpus == -1:
+    cpus = mp.cpu_count()
+else:
+    cpus = args.cpus
     
-  # Copy to images directory
-  print('Copying images')
-  for sample in tqdm(input_data):
-    src = os.path.join(args.pmc_output_path, sample['image_file_path'])
-    dst = os.path.join(args.images_output_path, sample['pair_id']+'.jpg')
-    shutil.copyfile(src, dst)
-      
+pool = Pool(cpus)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_path', type=str, default='data/llava_med_image_urls.jsonl')
-    parser.add_argument('--pmc_output_path', type=str, default='data/pmc_articles/')
-    parser.add_argument('--images_output_path', type=str, default='data/images/')
-    args = parser.parse_args()
-    main(args)
+pool.map(download_func, range(0, len(input_data)))
